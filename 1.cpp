5873aa8476435a17ae142915e49d0f4f0f91bf03
Augustas-Sujatas <augustas.sujatas@gmail.com>
2026-02-10 13:34:01 +0200

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 1. Studentas
+    string vardas = "Augustas";
+    string pavarde = "Sujatas";
+    int amzius = 19;
+    string grupe = "IS25";
+    int kursas = 1;
+    string studijuPrograma = "Informatika";
+
+    cout << " " << vardas << " " << pavarde << " " << amzius << " " << grupe << " " << kursas  << " " << studijuPrograma;
+    return 0;
+}
